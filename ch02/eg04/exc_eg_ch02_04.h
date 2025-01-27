b0ff9dff74ac09ebe30d78eea6c74bcf6e11cf30
Sivakumar Balasubramanian <siva82kb@gmail.com>
2025-01-27 09:16:31 +0530

    Completed chapter 02

---
+++ ch02/eg04/exc_eg_ch02_04.h
@@ -0,0 +1,7 @@
+#ifndef EXC_EG_CH02_04_H
+#define EXC_EG_CH02_04_H
+
+int add(int, int);
+int multiply(int, int);
+
+#endif
---
+++ ch02/eg04/exc_eg_ch02_04_main.c
@@ -0,0 +1,7 @@
+#include "exc_eg_ch02_04.h"
+
+int main(int argc, char** argv) {
+    int x = add(4, 5);
+    int y = multiply(9, x);
+    return 0;
+}
